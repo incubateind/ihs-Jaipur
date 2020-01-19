8034e50d6c5987a0f1f9a25b1cce4c98b752678b
Akshay Khandelwal <51502745+ak-akshay@users.noreply.github.com>
2020-01-19 10:31:43 +0530

    Add files via upload

---
+++ CsWarriors/streetvendor/firebase_config.js
@@ -0,0 +1,15 @@
+// Web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDup2ZrHsw2cN33dIl5uT-6BC7x16uE3kY",
+  authDomain: "streetvendor-6d116.firebaseapp.com",
+  databaseURL: "https://streetvendor-6d116.firebaseio.com",
+  projectId: "streetvendor-6d116",
+  storageBucket: "streetvendor-6d116.appspot.com",
+  messagingSenderId: "106658404229",
+  appId: "1:106658404229:web:90283f4f688ea4cddb96ec",
+  measurementId: "G-HV3WRZ7MFY"
+};
+
+
+  // Initialize Firebase
+  var app = firebase.initializeApp(firebaseConfig);
